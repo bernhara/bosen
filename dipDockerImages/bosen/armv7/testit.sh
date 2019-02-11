c759f003a5f05c6b05a43e607f5aed46fffcc8c6
bernhara <raphael.bernhard@orange.com>
2019-02-11 15:23:35 +0100

    reduce learning epochs to speadup test add test file

--- dipDockerImages/bosen/armv7/testit.sh
+++ dipDockerImages/bosen/armv7/testit.sh
@@ -7,18 +7,19 @@ echo "0 localhost 9999" > /tmp/sole_localhost_hostfile
 
 # train file is the one embedded in the container
 export train_file="/home/dip/datasets/covtype.scale.train.small"
+export test_file="/home/dip/datasets/covtype.scale.test.small"
 export num_app_threads=${NB_THREADS}
 
 export num_comm_channels_per_client=1
 export num_clients=1
 export client_id=0
 export global_data=true
-export perform_test=false
+export perform_test=true
 export hostfile="/tmp/sole_localhost_hostfile"
 export output_file_prefix="/tmp/direct_mlr_out"
 export num_test_eval=20
-export num_batches_per_epoch=10
-export num_epochs=40
+export num_batches_per_epoch=5
+export num_epochs=20
 export lr_decay_rate=0.99
 export num_train_eval=10000
 export init_lr=0.01
@@ -27,4 +28,4 @@ export lambda=0
 
 export mlr="${MLR_MAIN}"
 
-/home/dip/bin/mlrWrapper.sh
+bash -x /home/dip/bin/mlrWrapper.sh
