2dea39a23cb129c5d6a3bd093fbc1b4e83a0dc38
BERNHARD Raphael <raphael.bernhard@orange.com>
2019-02-18 17:47:55 +0100

    add comments
    remove skip-apt

--- dipDockerImages/bosen/armv7/build.sh
+++ dipDockerImages/bosen/armv7/build.sh
@@ -5,6 +5,10 @@
 : ${tmp_root:="${HERE}/tmp_root"}
 : ${REGISTRY_HOSTNAME_IMAGE_NAME_PREFIX="s-eunuc:5000"}
 
+#
+# Create the file system which will be copied to the container
+#
+
 rm -rf "${tmp_root}"
 
 mkdir -p "${tmp_root}/home/dip"
@@ -25,17 +29,16 @@ cp -a \
     "${tmp_root}/home/dip/bin"
 
 
+#
+# build image
+#
+
 image_name="${REGISTRY_HOSTNAME_IMAGE_NAME_PREFIX}/dip/mlr-worker"
 image_tag="latest"
 
 build_arg_element=""
 build_arg_element="${build_arg_element} http_proxy=http://proxy:8080"
 build_arg_element="${build_arg_element} https_proxy=http://proxy:8080"
-
-if [ -n "${skip_apt}" ]
-then
-    build_arg_element="${build_arg_element} skip_apt=true"
-fi
 
 build_arg_switch_list=""
 for a in ${build_arg_element}
