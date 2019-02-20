83d4375eb46785611744d6c2dc2c77cbc9f9cf4b
bernhara <raphael.bernhard@orange.com>
2019-02-20 09:46:35 +0100

    typo

--- dipDockerImages/bosen/armv7/testit.sh
+++ dipDockerImages/bosen/armv7/testit.sh
@@ -1,31 +1,27 @@
 #! /bin/bash
 
-mlr="${MLR_MAIN}"
-
 echo "0 localhost 9999" > /tmp/sole_localhost_hostfile
 
-
 # train file is the one embedded in the container
-export train_file="/home/dip/datasets/covtype.scale.train.small"
-export test_file="/home/dip/datasets/covtype.scale.test.small"
-export num_app_threads=${NB_THREADS}
-
-export num_comm_channels_per_client=1
-export num_clients=1
-export client_id=0
-export global_data=true
-export perform_test=true
-export hostfile="/tmp/sole_localhost_hostfile"
-export output_file_prefix="/tmp/direct_mlr_out"
-export num_test_eval=20
-export num_batches_per_epoch=5
-export num_epochs=20
-export lr_decay_rate=0.99
-export num_train_eval=10000
-export init_lr=0.01
-export num_batches_per_eval=10
-export lambda=0
 
 export mlr="${MLR_MAIN}"
 
-bash -x /home/dip/bin/mlrWrapper.sh
+VERBOSE=1 /home/dip/bin/mlrWrapper.sh \
+  --train_file="/home/dip/datasets/covtype.scale.train.small" \
+  --test_file="/home/dip/datasets/covtype.scale.test.small" \
+  --num_app_threads=${NB_THREADS} \
+  --num_comm_channels_per_client=1 \
+  --num_clients=1 \
+  --client_id=0 \
+  --global_data=true \
+  --perform_test=true \
+  --hostfile="/tmp/sole_localhost_hostfile" \
+  --output_file_prefix="/tmp/direct_mlr_out" \
+  --num_test_eval=20 \
+  --num_batches_per_epoch=5 \
+  --num_epochs=20 \
+  --lr_decay_rate=0.99 \
+  --num_train_eval=10000 \
+  --init_lr=0.01 \
+  --num_batches_per_eval=10 \
+  --lambda=0
