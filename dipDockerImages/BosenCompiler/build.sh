e695553d76f51cc8ec902eed73c7e2cf7aeb5d9d
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-09-12 17:47:21 +0200

    only pull if folder exists

--- dipDockerImages/BosenCompiler/build.sh
+++ dipDockerImages/BosenCompiler/build.sh
@@ -5,6 +5,30 @@
 HERE=$( dirname "$0" )
 ABS_HERE=$( readlink -f "${HERE}" )
 
+: ${tmp_root:="${HERE}/tmp_root"}
+
+: ${git_branch:=log-minibatch-data}
+
+if [ -d "${tmp_root}/bosen" ]
+then
+    (
+	cd "${tmp_root}/bosen"
+	http_proxy=http://proxy:3128 \
+	    https_proxy=http://proxy:3128 \
+	    git pull
+    )
+else
+
+    mkdir -p "${tmp_root}"
+    (
+	cd "${tmp_root}"
+	http_proxy=http://proxy:3128 \
+	    https_proxy=http://proxy:3128 \
+	    git clone -b "${git_branch}" https://github.com/bernhara/bosen.git
+    )
+fi
+
+
 image_name=dip_bosen_compiler
 image_tag=latest
 
