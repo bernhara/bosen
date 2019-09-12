af8cafcf2d656f886698aebde49530da17da92cc
BERNHARD Raphaël <raphael.bernhard@orange.com>
2019-09-12 17:26:33 +0200

    remove extraction of build result

--- dipDockerImages/BosenCompiler/build.sh
+++ dipDockerImages/BosenCompiler/build.sh
@@ -16,18 +16,3 @@ docker build  \
     --build-arg https_proxy=http://proxy:3128 \
     --file "${HERE}/Dockerfile" \
     ${HERE}
-
-rm -rf ${HERE}/tmp_out
-mkdir ${HERE}/tmp_out
-
-docker run \
-    --rm \
-    -v ${ABS_HERE}/tmp_out:/tmp_out \
-    "${image_name}:${image_tag}" \
-    /bin/bash -c "cp -r /PETUUM/bosen/app/mlr/bin /tmp_out"
-
-
-
-#    /bin/bash -c "cp -r /PETUUM/bosen/app/mlr/bin /tmp_out; chmod -R a+rwx /tmp_out/*"
-
-
